2e6923ac1a44124da1d4a884d3f2d60f471d1ce9
Naptie <naptie@hotmail.com>
2023-01-20 17:22:28 +0800

    1.0.2

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE = 'http://localhost:8000';
+export const API_BASE = import.meta.env.VITE_API_BASE;
 export const PAGINATION_PER_PAGE = 12
 export const USER_LEVELS = [
     {exp: 10, level: 1},
