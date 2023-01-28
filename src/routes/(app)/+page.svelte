a2912e1cadb13631c1107b1f3df2ea3895603ad7
Naptie <naptie@hotmail.com>
2023-01-28 17:24:55 +0800

    1.2.9.9

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -54,7 +54,7 @@
 						d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
 					/></svg
 				>
-				<span>{content.message}</span>
+				<span class="content">{content.message}</span>
 			</div>
 		</div>
 	</div>
