ebcb976ecb81887e7049c901120cd42e0ae088ab
Naptie <naptie@hotmail.com>
2023-07-05 00:44:35 +0800

    pass access_token when retrieving headline

--- src/routes/(app)/+page.ts
+++ src/routes/(app)/+page.ts
@@ -1,7 +1,7 @@
 import * as api from '$lib/api';
 
-export const load: import('./$types').PageLoad = async ({ fetch }) => {
-  const resp = await api.GET('/headline/', undefined, undefined, fetch);
+export const load: import('./$types').PageLoad = async ({ fetch, parent }) => {
+  const resp = await api.GET('/headline/', (await parent()).access_token, undefined, fetch);
   let json;
   if (resp.status === 200) {
     json = await resp.json();
