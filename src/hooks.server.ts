5200a81c9fc10235798c20e9298f8c8f2c45de3a
Naptie <naptie@qq.com>
2023-09-10 01:19:31 +0800

    refactor: add debug

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,6 +5,12 @@ import { locale } from '$lib/translations/config';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle = (async ({ event, resolve }) => {
+  console.log(
+    '[DBG] handle invoked with',
+    event.locals.user?.userName,
+    'and access_token',
+    event.locals.accessToken,
+  );
   console.log(event.url.pathname);
   let accessToken = event.cookies.get('access_token'),
     refreshToken = event.cookies.get('refresh_token');
@@ -18,6 +24,12 @@ export const handle = (async ({ event, resolve }) => {
       grant_type: 'refresh_token',
       refresh_token: refreshToken,
     });
+    console.log(
+      '[DBG] token refreshed for',
+      event.locals.user?.userName,
+      'with IP',
+      event.getClientAddress(),
+    );
 
     if (resp.ok) {
       ({ access_token: accessToken, refresh_token: refreshToken } = await resp.json());
@@ -28,12 +40,20 @@ export const handle = (async ({ event, resolve }) => {
       if (resp.ok) {
         event.locals.user = (await resp.json()).data;
         event.locals.lastRetrieval = Date.now();
+        console.log(
+          '[DBG] obtained new user',
+          event.locals.user?.userName,
+          'with IP',
+          event.getClientAddress(),
+        );
       }
     } else {
       event.locals.user = undefined;
+      console.log('[DBG] token refresh failed', 'with IP', event.getClientAddress());
     }
   } else {
     event.locals.user = undefined;
+    console.log('[DBG] no refresh_token present', 'with IP', event.getClientAddress());
   }
 
   if (event.locals.user) {
