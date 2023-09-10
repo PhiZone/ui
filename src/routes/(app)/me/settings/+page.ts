29325fa1876fb975ae2d4480d79dd0e009a3c88c
Naptie <naptie@qq.com>
2023-09-10 18:17:08 +0800

    fix(settings): user is not defined

---
+++ src/routes/(app)/me/settings/+page.ts
@@ -0,0 +1,7 @@
+import { error } from '@sveltejs/kit';
+import { t } from '$lib/translations/config';
+
+export const load = async ({ parent }) => {
+  const { user } = await parent();
+  if (!user) throw error(401, t.get('errors.401'));
+};
