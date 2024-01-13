bf3a5ddc40652cdf891431ba09be60b0a11f25b6
Naptie <naptie@qq.com>
2024-01-13 21:32:35 +0800

    fix: update success

--- src/lib/components/UpdateSuccess.svelte
+++ src/lib/components/UpdateSuccess.svelte
@@ -4,20 +4,20 @@
   export let id = 'update-success';
   export let checked: boolean;
   export let onClick: () => void;
-  export let title = $t('common.success');
-  export let content = $t('common.update_success');
-  export let buttonText = $t('common.confirm');
+  export let title: string | undefined = undefined;
+  export let content: string | undefined = undefined;
+  export let buttonText: string | undefined = undefined;
 </script>
 
 <input type="checkbox" {id} class="modal-toggle" {checked} />
 <div class="modal">
   <div class="modal-box">
-    <h3 class="font-bold text-lg">{title}</h3>
-    <p class="py-4">{content}</p>
+    <h3 class="font-bold text-lg">{title ?? $t('common.success')}</h3>
+    <p class="py-4">{content ?? $t('common.update_success')}</p>
     <div class="modal-action">
       <!-- svelte-ignore a11y-no-static-element-interactions -->
       <btn class="btn btn-success border-2 btn-outline" on:click={onClick} on:keyup>
-        {buttonText}
+        {buttonText ?? $t('common.confirm')}
       </btn>
     </div>
   </div>
