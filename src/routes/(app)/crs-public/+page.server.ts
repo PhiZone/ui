5f778a9a8c1b6cde5131f042a2d5d4b5cc1151ff
Naptie <naptie@qq.com>
2024-02-19 14:21:16 +0800

    feat: crs redirect when token available

--- src/routes/(app)/crs-public/+page.server.ts
+++ src/routes/(app)/crs-public/+page.server.ts
@@ -1,6 +1,13 @@
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
+import { OFFICIAL_SECTOKEN, FANMADE_SECTOKEN } from '$env/static/private';
 
-export const load = async ({ locals }) => {
+export const load = async ({ locals, cookies }) => {
+  if (
+    cookies.get('sec_token') == OFFICIAL_SECTOKEN ||
+    cookies.get('sec_token') == FANMADE_SECTOKEN
+  ) {
+    throw redirect(302, '/crs');
+  }
   if (!locals.user) {
     throw error(401, 'Unauthorized');
   }
