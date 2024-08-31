a0565570e2b7ad5f23da4b53902c75080aafd5d1
Naptie <naptie@qq.com>
2024-09-01 00:26:48 +0800

    refactor: event leaderboard

--- src/routes/(app)/events/divisions/[id=guid]/+page.ts
+++ src/routes/(app)/events/divisions/[id=guid]/+page.ts
@@ -3,6 +3,8 @@ import queryString from 'query-string';
 export const load = async ({ params, url, parent }) => {
   const { api, queryClient } = await parent();
   const searchParams = queryString.parse(url.search, { parseNumbers: true, parseBooleans: true });
+  searchParams.topRange = searchParams.topRange ?? 100;
+  searchParams.neighborhoodRange = searchParams.neighborhoodRange ?? 10;
 
   await Promise.allSettled([
     queryClient.prefetchQuery(api.event.division.info({ id: params.id })),
