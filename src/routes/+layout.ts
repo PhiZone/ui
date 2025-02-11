bfaf83c4ddcec9e180c4fee82087fca607b97232
Naptie <naptie@qq.com>
2025-02-12 02:46:21 +0800

    refactor: add vercel adapter

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,6 +4,12 @@ import { browser } from '$app/environment';
 import API from '$lib/api';
 import { loadTranslations, locale } from '$lib/translations/config';
 
+import type { Config } from '@sveltejs/adapter-vercel';
+
+export const config: Config = {
+  runtime: 'edge',
+};
+
 export const load = async ({ url, data, fetch }) => {
   await loadTranslations(data.language, url.pathname);
   locale.set(data.language);
