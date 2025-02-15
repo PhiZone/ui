1feac1a144a0fe1b03439e7bb54e814c5999c62e
Naptie <naptie@qq.com>
2025-02-15 16:02:03 +0800

    chore: update player-worker.sh

--- player-worker.sh
+++ player-worker.sh
@@ -2,11 +2,16 @@
 cd .. &&
     git clone https://github.com/PhiZone/player.git &&
     cd player &&
-    find src -type f -exec sed -i 's/\$lib/\$lib\/player/g' {} + && find src -type f -exec sed -i 's/\$\\{base\\}/player/g' {} + && find src -type f -exec sed -i 's/\\{base\\}/player/g' {} + && cd ../ui &&
+    find src -type f -exec sed -i 's/\$lib/\$lib\/player/g' {} + &&
+    find src -type f -exec sed -i 's/\$\\{base\\}/player/g' {} + &&
+    find src -type f -exec sed -i 's/\\{base\\}/player/g' {} + &&
+    cd ../repo &&
     mkdir src/routes/\(player\) &&
     mkdir src/routes/\(player\)/player &&
     cp -r ../player/src/routes/* src/routes/\(player\)/player/ &&
     mkdir src/lib/player &&
     cp -r ../player/src/lib/* src/lib/player/ &&
     mkdir static/player &&
-    cp -r ../player/static/* static/player/
+    cp -r ../player/static/* static/player/ &&
+    pnpm i -D @capacitor/android @capacitor/app @capacitor/cli @capacitor/core @capacitor/ios @tauri-apps/cli globals terser &&
+    pnpm i @awesome-cordova-plugins/android-full-screen @capacitor/clipboard @capacitor/filesystem @ffmpeg/ffmpeg @ffmpeg/util @fix-webm-duration/fix @sentry/sveltekit @tauri-apps/api @tauri-apps/plugin-deep-link @tauri-apps/plugin-fs @tauri-apps/plugin-os @tauri-apps/plugin-upload apng-js bezier-easing context-filter-polyfill cordova-plugin-fullscreen file-type gifuct-js jszip mathjs mime notiflix phaser phaser3-rex-plugins send-intent wavesurfer.js
