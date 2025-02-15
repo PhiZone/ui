d6179d1aecac1373756fc9a5ae28646bcaab5be7
Naptie <naptie@qq.com>
2025-02-15 18:30:35 +0800

    chore: update player-worker.sh

--- player-worker.sh
+++ player-worker.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+TAILWIND_CONFIG_INSERTION="
+      backgroundSize: {
+        'size-200': '200% 200%',
+      },
+      backgroundPosition: {
+        'pos-0': '2% 2%',
+        'pos-100': '98% 98%',
+      },
+    "
+
 cd .. &&
     git clone https://github.com/PhiZone/player.git &&
     cd player &&
@@ -15,5 +26,6 @@ cd .. &&
     mkdir static/player &&
     cp -r ../player/static/* static/player/ &&
     rm -rf static/player/.well-known static/player/ffmpeg &&
+    sed -i "11s/^\(.\{14\}\)/\1${TAILWIND_CONFIG_INSERTION}/" tailwind.config.ts &&
     pnpm i -D @capacitor/android @capacitor/app @capacitor/cli @capacitor/core @capacitor/ios @tauri-apps/cli globals terser &&
     pnpm i @awesome-cordova-plugins/android-full-screen @capacitor/clipboard @capacitor/filesystem @ffmpeg/ffmpeg @ffmpeg/util @fix-webm-duration/fix @sentry/sveltekit @tauri-apps/api @tauri-apps/plugin-deep-link @tauri-apps/plugin-fs @tauri-apps/plugin-os @tauri-apps/plugin-upload apng-js bezier-easing context-filter-polyfill cordova-plugin-fullscreen file-type gifuct-js jszip mathjs mime notiflix phaser phaser3-rex-plugins send-intent wavesurfer.js
