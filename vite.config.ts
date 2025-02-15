4410daafdf40a714327e28472d4ca5d05284c11f
Naptie <naptie@qq.com>
2025-02-15 15:42:00 +0800

    chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,24 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { exec } from 'child_process';
+
+exec(
+  "cd .. \
+  && git clone https://github.com/PhiZone/player.git \
+  && cd player \
+  && find src -type f -exec sed -i 's/\$lib/\$lib\/player/g' {} +\
+  && find src -type f -exec sed -i 's/\$\{base\}/player/g' {} +\
+  && find src -type f -exec sed -i 's/\{base\}/player/g' {} +\
+  && cd ../ui \
+  && mkdir src/routes/\(player\) \
+  && mkdir src/routes/\(player\)/player \
+  && cp -r ../player/src/routes/* src/routes/\(player\)/player/ \
+  && mkdir src/lib/player \
+  && cp -r ../player/src/lib/* src/lib/player/ \
+  && mkdir static/player \
+  && cp -r ../player/static/* static/player/ \
+  ",
+);
 
 export default defineConfig({
   plugins: [sveltekit()],
